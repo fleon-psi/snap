e5db1311345a806bc205093f7166d6a8768589aa
fleon-psi <filip-karol.leonarski@psi.ch>
2020-01-10 13:06:24 +0100

    Action code modifications
    Signed-off-by: fleon-psi <filip-karol.leonarski@psi.ch>

--- actions/hls_rx100G/hw/hw_action_rx100G.cpp
+++ actions/hls_rx100G/hw/hw_action_rx100G.cpp
@@ -18,20 +18,25 @@
 #include "ap_int.h"
 #include "hw_action_rx100G.h"
 
-enum rcv_state_t {RCV_INIT, RCV_GOOD, RCV_IGNORE};
+enum rcv_state_t {RCV_INIT, RCV_GOOD, RCV_BAD, RCV_IGNORE};
 
+inline void mask_tkeep(ap_uint<512> &data, ap_uint<64> keep) {
+#pragma HLS UNROLL
+	for (int i = 0; i < 64; i++) {
+		if (keep[i] == 0) data(i*8+7,i*8) = 0;
+	}
+}
 //----------------------------------------------------------------------
 //--- MAIN PROGRAM -----------------------------------------------------
 //----------------------------------------------------------------------
 static int process_action(snap_membus_t *din_gmem,
 		snap_membus_t *dout_gmem,
 		AXI_STREAM &din_eth,
-		//		AXI_STREAM &dout_eth,
 		action_reg *act_reg)
 {
 	uint64_t o_idx = 0;
 	uint64_t out_offset = act_reg->Data.out.addr >> ADDR_RIGHT_SHIFT;
-size_t bytes_read;
+	size_t bytes_read = 0;
 
 	ap_axiu_for_eth packet_in;
 
@@ -44,10 +49,12 @@ size_t bytes_read;
 	act_reg->Data.good_packets = 0;
 	act_reg->Data.ignored_packets = 0;
 	act_reg->Data.bad_packets = 0;
+	act_reg->Data.user = 0;
 
 	while (packets_read < act_reg->Data.packets_to_read) {
 #pragma HLS PIPELINE
 		din_eth.read(packet_in);
+		//if (packet_in.last == 1) rcv_state = RCV_IGNORE;
 		switch (rcv_state) {
 		case RCV_INIT:
 			decode_eth_1(packet_in.data, packet_header);
@@ -55,7 +62,8 @@ size_t bytes_read;
 					(packet_header.ether_type == 0x0800) && // IP
 					(packet_header.ip_version == 4) && // IPv4
 					(packet_header.ipv4_protocol == 0x11) && // UDP
-					(packet_header.ipv4_total_len == 8268)) // Packet length is consistent with JUNGFRAU
+					(packet_header.ipv4_total_len == 8268) && // Packet length is consistent with JUNGFRAU
+					(packet_in.last == 0))
 			{
 				rcv_state = RCV_GOOD;
 				memcpy(dout_gmem + out_offset + o_idx, (char *) (&packet_in.data), BPERDW);
@@ -66,29 +74,35 @@ size_t bytes_read;
 				act_reg->Data.ignored_packets++;
 			}
 			break;
-		case RCV_IGNORE:
-			break;
+
 		case RCV_GOOD:
-			if (o_idx > packet_header.ipv4_total_len/64+2) {
-				rcv_state = RCV_IGNORE;
-				act_reg->Data.bad_packets++;
-			}
+			if (o_idx >= 130) rcv_state = RCV_BAD; // Cannot receive more than 130
 			else {
-				memcpy(dout_gmem + out_offset + o_idx, (char *) (&packet_in.data), BPERDW);
+				ap_uint<512> tmp = packet_in.data;
+				mask_tkeep(tmp,packet_in.keep);
+				memcpy(dout_gmem + out_offset + o_idx, (char *) (&tmp), BPERDW);
 				o_idx++;
 				bytes_read += 64;
 			}
 			break;
+		case RCV_IGNORE:
+				case RCV_BAD:
+					break;
 		}
 		if (packet_in.last == 1) {
-			if ((rcv_state == RCV_GOOD) || (rcv_state == RCV_IGNORE)) packets_read++;
-			if (rcv_state == RCV_GOOD) {
+			if ((rcv_state == RCV_GOOD) || (rcv_state == RCV_BAD)) packets_read++;
+			if ((rcv_state == RCV_BAD) ||
+					((rcv_state == RCV_GOOD) && (packet_in.user == 1)) ||
+					((rcv_state == RCV_GOOD) && (o_idx != 130))) {
+				act_reg->Data.bad_packets++;
+				act_reg->Data.user += packet_in.user;
+				o_idx = 0; // offset stays the same, so bad data will be overwritten
+			} else if (rcv_state == RCV_GOOD) {
 				act_reg->Data.good_packets++;
-				out_offset += o_idx + 1;
+				out_offset += 130;
 				o_idx = 0;
 			}
 			rcv_state = RCV_INIT;
-			act_reg->Data.user = packet_in.user;
 		}
 	}
 
@@ -102,7 +116,6 @@ size_t bytes_read;
 void hls_action(snap_membus_t *din_gmem,
 		snap_membus_t *dout_gmem,
 		AXI_STREAM &din_eth,
-		//		AXI_STREAM &dout_eth,
 		/* snap_membus_t *d_ddrmem, // CAN BE COMMENTED IF UNUSED */
 		action_reg *act_reg,
 		action_RO_config_reg *Action_Config)
